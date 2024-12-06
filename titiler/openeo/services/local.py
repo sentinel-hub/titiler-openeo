ed5156005c5d71fd68f3d4a36e1d0f52a14edcaa
vincentsarago <vincent.sarago@gmail.com>
2024-12-06 13:14:40 +0100

    sketch out tiler service

--- titiler/openeo/services/local.py
+++ titiler/openeo/services/local.py
@@ -16,40 +16,122 @@ default_services = {
     "d55ae9e5-83d7-41c2-ae56-e6c72d3b7da5": {
         "user_id": "anonymous",
         "service": {
-            "title": "test",
-            "description": None,
+            "parameters": [
+                {"description": "", "name": "spatial_extent_east", "schema": {}},
+                {"description": "", "name": "spatial_extent_north", "schema": {}},
+                {"description": "", "name": "spatial_extent_south", "schema": {}},
+                {"description": "", "name": "spatial_extent_west", "schema": {}},
+            ],
             "type": "XYZ",
+            "title": "simple_3-bands_rgb_dynamic_bbox",
             "enabled": True,
-            "configuration": {},
-            "plan": "trial",
-            "budget": None,
-            "process": {
-                "process_graph": {
-                    # TODO: create proper graph
-                    "load1": {
-                        "process_id": "load_collection",
-                        "arguments": {
-                            "id": "SENTINEL2_L2A",
-                            "spatial_extent": {
-                                "west": -10.740723669048181,
-                                "east": 20.93035261581019,
-                                "south": 38.250605037486906,
-                                "north": 54.47085481675458,
-                            },
-                            "temporal_extent": ["2016-11-01T00:00:00Z", None],
-                            "bands": ["B01"],
+            "process_graph": {
+                "1": {
+                    "arguments": {
+                        "bands": ["B04", "B03", "B02"],
+                        "id": "SENTINEL2_L2A",
+                        "spatial_extent": {
+                            "east": {"from_parameter": "spatial_extent_east"},
+                            "north": {"from_parameter": "spatial_extent_north"},
+                            "south": {"from_parameter": "spatial_extent_south"},
+                            "west": {"from_parameter": "spatial_extent_west"},
+                        },
+                        "temporal_extent": [
+                            "2022-03-26T00:00:00Z",
+                            "2022-03-26T23:59:59Z",
+                        ],
+                    },
+                    "process_id": "load_collection",
+                },
+                "2": {
+                    "arguments": {
+                        "data": {"from_node": "3"},
+                        "format": "PNG",
+                        "options": {"datatype": "byte"},
+                    },
+                    "process_id": "save_result",
+                    "result": True,
+                },
+                "3": {
+                    "arguments": {
+                        "data": {"from_node": "1"},
+                        "process": {
+                            "process_graph": {
+                                "1": {
+                                    "arguments": {
+                                        "inputMax": 0.4,
+                                        "inputMin": 0,
+                                        "outputMax": 255,
+                                        "x": {"from_parameter": "x"},
+                                    },
+                                    "process_id": "linear_scale_range",
+                                    "result": True,
+                                }
+                            }
                         },
                     },
-                    "save1": {
-                        "process_id": "save_result",
-                        "arguments": {"format": "PNG", "data": {"from_node": "load1"}},
-                        "result": True,
-                    },
+                    "process_id": "apply",
                 },
-                "parameters": [],
             },
         },
-    }
+    },
+    "31cbbbc1-9095-42f8-b4a5-fd36a653adaa": {
+        "user_id": "anonymous",
+        "service": {
+            "parameters": [],
+            "title": "simple_3-bands_rgb",
+            "type": "XYZ",
+            "enabled": True,
+            "process_graph": {
+                "1": {
+                    "process_id": "load_collection",
+                    "arguments": {
+                        "bands": ["B05", "B04", "B03"],
+                        "id": "SENTINEL2_L2A",
+                        "spatial_extent": {
+                            "west": 14.503132250376241,
+                            "south": 45.98989222284457,
+                            "east": 14.578437275398317,
+                            "north": 46.04381770188389,
+                        },
+                        "temporal_extent": [
+                            "2022-03-26T00:00:00Z",
+                            "2022-03-26T23:59:59Z",
+                        ],
+                    },
+                },
+                "2": {
+                    "process_id": "save_result",
+                    "arguments": {
+                        "data": {"from_node": "3"},
+                        "format": "PNG",
+                        "options": {"datatype": "byte"},
+                    },
+                    "result": True,
+                },
+                "3": {
+                    "process_id": "apply",
+                    "arguments": {
+                        "data": {"from_node": "1"},
+                        "process": {
+                            "process_graph": {
+                                "1": {
+                                    "process_id": "linear_scale_range",
+                                    "arguments": {
+                                        "inputMax": 0.4,
+                                        "inputMin": 0,
+                                        "outputMax": 255,
+                                        "x": {"from_parameter": "x"},
+                                    },
+                                    "result": True,
+                                }
+                            }
+                        },
+                    },
+                },
+            },
+        },
+    },
 }
 
 
