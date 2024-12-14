53ea33bf27b706845949aca34f5810008535110c
vincentsarago <vincent.sarago@gmail.com>
2024-12-14 14:32:07 +0100

    fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,10 +11,8 @@ DATA_DIR = os.path.join(os.path.dirname(__file__), "fixtures")
 @pytest.fixture(autouse=True)
 def app(monkeypatch) -> TestClient:
     """Create App."""
-    monkeypatch.setenv(
-        "TITILER_OPENEO_STAC_API_URL", "https://stac.dataspace.copernicus.eu/v1"
-    )
-
+    monkeypatch.setenv("TITILER_OPENEO_STAC_API_URL", "https://stac.eoapi.dev")
+    monkeypatch.setenv("TITILER_OPENEO_SERVICE_STORE_URL", "services/eoapi.json")
     from titiler.openeo.main import app
 
     return TestClient(app)
