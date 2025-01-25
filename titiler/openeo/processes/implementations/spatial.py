a2a23a7af3e0083ff735c227c554e60281fb6f11
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2025-01-25 21:35:35 +0100

    Refactor resample_spatial function to include projection, resolution, and method parameters for improved image reprojection

--- titiler/openeo/processes/implementations/spatial.py
+++ titiler/openeo/processes/implementations/spatial.py
@@ -1,6 +1,9 @@
 """titiler.openeo.processes Spatial."""
 
-from typing import Callable, Dict, Optional, Union
+from typing import Tuple, Union
+
+from pyproj import CRS
+from rasterio.warp import Resampling
 
 from .data_model import ImageData, RasterStack
 
@@ -9,36 +12,51 @@ __all__ = ["resample_spatial"]
 
 def resample_spatial(
     data: Union[RasterStack, ImageData],
-    process: Callable,
-    context: Optional[Dict] = None,
+    projection: Union[int, str],
+    resolution: Union[float, Tuple[float, float]],
+    align: str,
+    method: str = "nearest",
 ) -> Union[RasterStack, ImageData]:
     """Resample and warp the spatial dimensions of the raster at a given resolution."""
-    positional_parameters = {"x": 0}
-    named_parameters = {"context": context}
 
-    def _reproject_img(img: ImageData):
-        
-        img.transform
-        ds = ds.rio.reproject(
-            dst_crs,
-            shape=(tilesize, tilesize),
-            transform=from_bounds(*tile_bounds, height=tilesize, width=tilesize),
-            resampling=Resampling[reproject_method],
-            nodata=nodata,
-        )
-        
-        return ImageData(
-            process(
-                img.array,
-                positional_parameters=positional_parameters,
-                named_parameters=named_parameters,
-            ),
-            assets=img.assets,
-            crs=img.crs,
-            bounds=img.bounds,
-        )
+    def _reproject_img(
+        img: ImageData, dst_crs: CRS, resolution: int, method: str
+    ) -> ImageData:
+        # align is not yet implemented
+        if align is not None:
+            raise NotImplementedError(
+                "resample_spatial: align parameter is not yet implemented"
+            )
+
+        dst_crs = CRS(projection)
+        # map resampling method to rio-tiler method using a dictionary
+        resampling_method: Resampling = {
+            "nearest": Resampling.nearest,
+            "bilinear": Resampling.bilinear,
+            "cubic": Resampling.cubic,
+            "cubicspline": Resampling.cubic_spline,
+            "lanczos": Resampling.lanczos,
+            "average": Resampling.average,
+            "mode": Resampling.mode,
+            "max": Resampling.max,
+            "min": Resampling.min,
+            "med": Resampling.med,
+            "q1": Resampling.q1,
+            "q3": Resampling.q3,
+            "sum": Resampling.sum,
+            "rms": Resampling.rms,
+            "near": None,
+        }[method]
+
+        res = resolution if isinstance(resolution, tuple) else (resolution, resolution)
+
+        # reproject the image
+        return img.reproject(dst_crs, res, resampling_method)
+
+    """ Get destination CRS from parameters """
+    dst_crs = CRS.from_epsg(projection)
 
     if isinstance(data, ImageData):
-        return _reproject_img(data)
+        return _reproject_img(data, dst_crs, resolution, method)
 
     return {k: _reproject_img(img) for k, img in data.items()}
