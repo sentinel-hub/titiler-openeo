a839eaddd37d42878ec1491063e6301ff420d824
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2025-04-15 21:47:58 +0200

    Enhance array_element function with index and label validation; improve error handling for data extraction

--- titiler/openeo/processes/implementations/arrays.py
+++ titiler/openeo/processes/implementations/arrays.py
@@ -11,26 +11,42 @@ from .data_model import LazyRasterStack, RasterStack
 __all__ = ["array_element", "to_image"]
 
 
-def array_element(data: Union[ArrayLike, RasterStack, LazyRasterStack], index: int):
+def array_element(
+    data: Union[ArrayLike, RasterStack, LazyRasterStack],
+    index: int = None,
+    label: str = None,
+) -> ArrayLike:
     """Return element from array.
 
     Args:
         data: Array or RasterStack to extract element from
         index: Index of the element to extract
+        label: Label of the element to extract (for RasterStack)
 
     Returns:
         The element at the specified index
     """
     if index is not None and index < 0:
         raise IndexError(f"Index value must be >= 0, {index}")
+    if label is not None and label not in data.keys():
+        raise KeyError(f"Label {label} not found in data: {data.keys()}")
+    if index is None and label is None:
+        raise ValueError("Either index or label must be provided")
 
     # Handle RasterStack
     if isinstance(data, dict):
-        array_dict = {k: numpy.take(v.array, index, axis=0) for k, v in data.items()}
+        if index is not None:
+            array_dict = {
+                k: numpy.take(v.array, index, axis=0) for k, v in data.items()
+            }
+        if label is not None:
+            array_dict = {k: v.array for k, v in data.items() if k == label}
         # return a multi-dimensional array
         return numpy.stack(list(array_dict.values()), axis=0)
 
     # Handle regular arrays
+    elif isinstance(data, ImageData):
+        return numpy.take(data.array, index, axis=0)
     else:
         return numpy.take(data, index, axis=0)
 
