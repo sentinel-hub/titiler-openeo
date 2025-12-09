7e7a0d2854adf0565bd28987d985def67de0d3c9
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2025-12-09 22:04:44 +0100

    Add max_items parameter to LoadCollection for enhanced item retrieval control

--- titiler/openeo/stacapi.py
+++ titiler/openeo/stacapi.py
@@ -229,6 +229,7 @@ class LoadCollection:
         properties: Optional[dict] = None,
         fields: Optional[List[str]] = None,
         limit: Optional[int] = None,
+        max_items: Optional[int] = None,
     ) -> List[Item]:
         fields = fields or [
             "assets",
@@ -245,6 +246,7 @@ class LoadCollection:
             "collections": [id],
             "fields": fields,
             "limit": limit,
+            "max_items": max_items,
         }
 
         if spatial_extent is not None:
