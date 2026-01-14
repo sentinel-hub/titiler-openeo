5866d1e42c2c056e3385d32f671ee562c7669c46
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2026-01-14 09:15:16 +0100

    chore: revert setuptools_scm from PR #166

--- titiler/openeo/__init__.py
+++ titiler/openeo/__init__.py
@@ -1,9 +1,3 @@
 """titiler.openeo"""
 
-try:  # pragma: no cover
-    from ._version import version as __version__
-except ImportError:  # pragma: no cover
-    # fallback for development installs
-    from importlib.metadata import version
-
-    __version__ = version("titiler-openeo")
+__version__ = "0.9.0"
